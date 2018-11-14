691ebeea4eabdebdda2053776492331a7a3d00a8
xjThree <xj250323489@163.com>
2018-11-14 18:13:29 +0800

    Initial commit

---
+++ Example/WDReactNative/WDAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  WDAppDelegate.h
+//  WDReactNative
+//
+//  Created by xjThree on 11/14/2018.
+//  Copyright (c) 2018 xjThree. All rights reserved.
+//
+
+@import UIKit;
+
+@interface WDAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
